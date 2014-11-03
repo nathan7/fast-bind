288e8b4339e373ed3d3e5248614735d3416d6506
Nathan Zadoks <nathan@nathan7.eu>
2014-11-03 19:17:26 +0100

    actually mostly adhere to spec

--- bind-slice.js
+++ bind-slice.js
@@ -1,17 +1,40 @@
 'use strict';
 module.exports = function(boundThis) {
   var f = this
-  if (arguments.length === 1)
-    return function() { return f.apply(boundThis, arguments) }
+    , ret
 
-  var boundArgs = new Array(arguments.length - 1)
-  for (var i = 1; i < arguments.length; i++)
-    boundArgs[i - 1] = arguments[i]
+  if (arguments.length < 2)
+    ret = function() {
+      if (this instanceof ret) {
+        var ret_ = f.apply(this, arguments)
+        return Object(ret_) === ret_
+          ? ret_
+          : this
+      }
+      else
+        return f.apply(boundThis, arguments)
+    }
+  else {
+    var boundArgs = new Array(arguments.length - 1)
+    for (var i = 1; i < arguments.length; i++)
+      boundArgs[i - 1] = arguments[i]
 
-  return function() {
-    var args = boundArgs.slice()
-    for (var i = 0; i < arguments.length; i++)
-      args.push(arguments[i])
-    return f.apply(boundThis, args)
+    ret = function() {
+      var args = boundArgs.slice()
+      for (var i = 0; i < arguments.length; i++)
+        args.push(arguments[i])
+
+      if (this instanceof ret) {
+        var ret_ = f.apply(this, arguments)
+        return Object(ret_) === ret_
+          ? ret_
+          : this
+      }
+      else
+        return f.apply(boundThis, args)
+    }
   }
+
+  ret.prototype = f.prototype
+  return ret
 }
